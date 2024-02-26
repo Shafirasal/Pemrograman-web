873f8714c7394b489327eef44898220c4e347764
shafira <shafirasalsabila15@gmail.com>
2024-02-26 09:57:53 +0700

    first commit

---
+++ pertemuan2/variabel.php
@@ -0,0 +1,41 @@
+<?php
+$name="Fitria ramadhani";
+$alamat="Malang";
+define("GAJI", 50000000);
+define("nomorrumah", 20);
+
+echo $name;
+echo "<br>";
+echo GAJI;
+echo "<br>";
+echo  nomorrumah;
+echo "<br>";
+echo"$name bergaji rp. GAJI";
+echo "<br>";
+echo"<hr>";
+echo "$name bergaji rp. " .GAJI;
+echo "<br>";
+echo "nama saya : $name";
+echo "<br>";
+echo "alamat rumah saya : " .$alamat;
+echo "<br>";
+echo "$name $alamat";
+echo "<br>";
+echo $name .$alamat;
+echo "<hr>";
+echo "Ini menggunakan unset";
+unset($name);
+echo "ini memakai unset $name";
+echo $nama;
+
+
+//echo mencetak
+// tanda petik: tiper char
+//menghubugkan string pake titik
+//$name, $nama
+//unset menghapus variabel () undefined tu keterangan menghapus
+//var_dump: melihat jumlah tipe datanya, digunakan jika terjadi error
+?>
+
+
+
---
+++ php/variabel.php
@@ -0,0 +1,12 @@
+<?php
+$name="antok";
+define("GAJI", 50000000);
+
+echo $name;
+echo "<br>";
+echo GAJI;
+
+
+
+
+?>
