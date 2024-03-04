8c09b24ced1592135b91650364532140e152ac81
shafira <shafirasalsabila15@gmail.com>
2024-03-04 12:55:55 +0700

    struktur kontrol

---
+++ pertemuan4_php/struktur_kontrol.php
@@ -0,0 +1,29 @@
+<?php
+
+//membuat kode kondisional if else dengan memasukan nilai numerik 92
+// $nilaiNumerik = 92;
+
+// if ($nilaiNumerik >=90 && $nilaiNumerik <= 100){
+//     echo "Nilai huruf: A";
+// } elseif ($nilaiNumerik >= 80 && $nilaiNumerik < 90){
+//     echo "Nilai huruf: B";
+// } elseif ($nilaiNumerik >=70 && $nilaiNumerik < 80){
+//     echo "Nilai huruf: C";
+// } elseif ($nilaiNumerik < 70){
+//     echo "Nilai huruf: D";
+// }
+
+
+// menggunakan looping (while) dala php untuk menghitung waktu yang dibutuhkan atlet untuk mencapai untuk mencapai jarak tertentu
+$jarakSaatIni = 0;
+$jarakTarget = 500;
+$peningkatanHarian = 30;
+$hari = 0;
+
+while ($jarakSaatIni < $jarakTarget){
+    $jarakSaatIni += $peningkatanHarian;
+    $hari++;
+}
+
+echo "Atlet tersebut memerlukan $hari hari untuk mencapai untuk mencapai jarak 500 kilometer"
+?>
