1e1f696e2b75ab685b73652379f03323bb381d9d
shafira <shafirasalsabila15@gmail.com>
2024-03-07 20:42:28 +0700

    update isi praktikum

--- pertemuan4_php/struktur_kontrol.php
+++ pertemuan4_php/struktur_kontrol.php
@@ -86,7 +86,7 @@ Guru ini ingin mengabaikan dua nilai tertinggi dan dua nilai terendah. Bantu gur
 //     echo "Tidak ada nilai yang dioperasikan.";
 // }
 
-
+//soal cerita yg sama seperti di ats
 $nilaiMatematika = [85, 92, 78, 64, 90, 75, 88, 79, 70, 96];
 $totalNilai = 0;
 $rataRata=0;
@@ -98,6 +98,27 @@ for($i=2; $i<8; $i++){
 }
 
 $rataRata = $totalNilai/(count($nilaiMatematika)-4);
-echo "nilai rata rata adalah $rataRata"
+echo "nilai rata rata adalah $rataRata";
+
+//soal cerita : Seorang pelanggan ingin membeli sebuah produk dengan harga Rp 120.000. Toko tersebut menawarkan diskon sebesar 20% untuk pembelian di atas Rp 100.000. Bantu pelanggan ini untuk menghitung harga yang harus dibayar setelah mendapatkan diskon.
+$harga = 120000;
+$diskon = 0.2; // 20% diskon
+$biaya = 0;
+if ($harga >= 100000) {
+    $biaya = $harga - ($harga * $diskon); // Hitung total biaya dengan diskon
+    echo "<hr> sehingga total biaya yang perlu dibayar oleh pembeli adalah: Rp.$biaya";  
+}
+
+/*Seorang pemain game ingin menghitung total skor mereka dalam permainan. 
+Mereka mendapatkan skor berdasarkan poin yang mereka kumpulkan. Jika mereka memiliki lebih dari 500 poin, maka mereka akan mendapatkan hadiah tambahan. Buat tampilan baris pertama “Total skor pemain adalah: (poin)”. Dan baris kedua “Apakah pemain mendapatkan hadiah tambahan? (YA/TIDAK)”
+ */
+
+$point = 550;
+$total_skor = $point;
+$hadiah_tambahan= ($total_skor > 500) ? "YA" : "TIDAK";
+
+echo "<hr>
+total skor pemail adalah $total_skor<br>";
+echo "Apakah pemain mendapatkan hadiah tambahan? $hadiah_tambahan";
 
 ?>
