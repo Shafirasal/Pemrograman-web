48a8b4fa355ba5c7c1ad01c85437035495c38f70
shafira <shafirasalsabila15@gmail.com>
2024-03-09 17:05:17 +0700

    update file tugas

--- pertemuan4_php/tugas array/arrayMultidimensi.php
+++ pertemuan4_php/tugas array/arrayMultidimensi.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Data Mahasiswa</title>
+    <title>Data Mahasiswa Shafira</title>
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -19,12 +19,12 @@
             object-fit: cover; 
             margin-right: 20px;
         }
-        .student-details {
+        .enhypen-details { /* Mengubah student-details menjadi enhypen-details */
             display: flex;
             flex-direction: column;
             margin: 10px 0;
         }
-        .student-details p {
+        .enhypen-details p { /* Mengubah student-details p menjadi enhypen-details p */
             margin: 0;
         }
         .symbol {
@@ -39,26 +39,25 @@
     <?php
     // Array multidimensi untuk data mahasiswa
     $mahasiswa = array(
-        array("Nama" => "Sunghoon", "NIM" => "881100", "Jurusan" => "Teknik Perkapalan", "Email" => "sunghoon@gmail.com", "enha" => "Sunghoon.jpg"),
-        array("Nama" => "Jay", "NIM" => "235680", "Jurusan" => "Teknik Informatika", "Email" => "jay@gmail.com", "enha" => "Jay.jpg"),
-        array("Nama" => "Jake", "NIM" => "198854", "Jurusan" => "Kedokteran", "Email" => "jake@gmail.com", "enha" => "Jake.jpg"),
-        array("Nama" => "Jungwon", "NIM" => "123456", "Jurusan" => "Teknik Mesin", "Email" => "jungwon@gmail.com", "enha" => "Jungwon.jpg"),
-        array("Nama" => "Niki", "NIM" => "010101", "Jurusan" => "Teknik Elektro", "Email" => "niki@gmail.com", "enha" => "Niki.jpg")
+        array("Nama" => "Sunghoon", "NIM" => "2241760001", "Jurusan" => "Teknik Perkapalan", "Email" => "sunghoon@gmail.com", "enha" => "Sunghoon.jpg"),
+        array("Nama" => "Jay", "NIM" => "2241760002", "Jurusan" => "Teknik Informatika", "Email" => "jay@gmail.com", "enha" => "Jay.jpg"),
+        array("Nama" => "Jake", "NIM" => "2241760005", "Jurusan" => "Kedokteran", "Email" => "jake@gmail.com", "enha" => "Jake.jpg"),
+        array("Nama" => "Jungwon", "NIM" => "2241760004", "Jurusan" => "Teknik Mesin", "Email" => "jungwon@gmail.com", "enha" => "Jungwon.jpg"),
+        array("Nama" => "Niki", "NIM" => "2241760006", "Jurusan" => "Teknik Elektro", "Email" => "niki@gmail.com", "enha" => "Niki.jpg")
     );
 
     // Loop melalui array untuk menampilkan data mahasiswa
-foreach ($mahasiswa as $data) {
-    echo '<div class="enhypen-container">';
-    echo '<img src="enha/' . $data["enha"] . '" alt="Foto Mahasiswa" class="pict-enha">'; //link folder "enha"
-    echo '<div class="student-details">'; // Mengisi detail mahasiswa
-    echo '<p><span class="symbol">•</span> Nama: ' . $data["Nama"] . '</p>';
-    echo '<p><span class="symbol">•</span> NIM: ' . $data["NIM"] . '</p>';
-    echo '<p><span class="symbol">•</span> Jurusan: ' . $data["Jurusan"] . '</p>';
-    echo '<p><span class="symbol">•</span> Email: ' . $data["Email"] . '</p>';
-    echo '</div>';
-    echo '</div>';
-}
-
+    foreach ($mahasiswa as $data) {
+        echo '<div class="enhypen-container">';
+        echo '<img src="enha/' . $data["enha"] . '" alt="Foto Mahasiswa" class="pict-enha">'; //link folder "enha"
+        echo '<div class="enhypen-details">'; // Mengisi detail mahasiswa
+        echo '<p><span class="symbol">•</span> Nama: ' . $data["Nama"] . '</p>';
+        echo '<p><span class="symbol">•</span> NIM: ' . $data["NIM"] . '</p>';
+        echo '<p><span class="symbol">•</span> Jurusan: ' . $data["Jurusan"] . '</p>';
+        echo '<p><span class="symbol">•</span> Email: ' . $data["Email"] . '</p>';
+        echo '</div>';
+        echo '</div>';
+    }
     ?>
 
 </body>
